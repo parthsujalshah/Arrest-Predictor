0b1f507a7f9eb82654ea15eae6701a20c25bf874
parthsujalshah <parthsujalshah@gmail.com>
2021-11-20 04:00:16 +0530

    complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Form, Button, Select, Card } from 'antd';
+import { Form, Button, Select, Card, Affix, PageHeader, Modal, Spin } from 'antd';
 import axios from "axios";
 import { apiUrl, apiGetColNames } from './constants';
+import Lottie from 'react-lottie';
+import * as animationArrested from './lottiefiles/arrested.json';
+import * as animationNotArrested from './lottiefiles/not-arrested.json';
 
 const { Option } = Select;
 
@@ -13,6 +16,28 @@ function App() {
   const [domesticField, setDomesticField] = React.useState([]);
   const [monthField, setMonthField] = React.useState([]);
   const [primaryTypeField, setPrimaryTypeField] = React.useState([]);
+  const [arrested, setArrested] = React.useState(false);
+  const [modalLoading, setModalLoading] = React.useState(false);
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
+  const [isStopped, setIsStopped] = React.useState(false);
+  const [isPaused, setIsPaused] = React.useState(false);
+
+  const defaultArrested = {
+    loop: false,
+    autoplay: true,
+    animationData: animationArrested.default,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+  const defaultNotArrested = {
+    loop: false,
+    autoplay: true,
+    animationData: animationNotArrested.default,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
   const dayMap = {
     "0": "MONDAY",
@@ -72,108 +97,174 @@ function App() {
     }
     getSetColValues();
   }, []);
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
   return (
-    <div style={{
-      alignItems: "center",
-      justifyContent: "center",
-      display: "flex"
-    }}>
-      <Card style={{
-        // backgroundColor: "#d9d9d9",
-        backgroundColor: "#ebf8f2",
-        width: "80vh",
+    <div>
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={() => {
+        setIsModalVisible(false);
+      }} onCancel={() => {
+        setIsModalVisible(false);
       }}>
-        <Form
-          onFinish={val => console.log(val)}
-          onFinishFailed={err => console.log(err)}
-        >
-          <Form.Item
-            name="community_area"
-            label="Community Area"
-            rules={[
+        {
+          modalLoading ?
+            <Spin size="large" />
+            :
+            <div>
               {
-                required: true,
-                message: 'Please select the community area',
-              },
-            ]}
+                arrested ?
+                  <div>
+                    <Lottie height={"400"} width={"400"} options={defaultArrested} isStopped={isStopped} isPaused={isPaused} />
+                    <h2>High chances criminals will be arrested</h2>
+                  </div>
+                  :
+                  <div>
+                    <Lottie height={"400"} width={"400"} options={defaultNotArrested} isStopped={isStopped} isPaused={isPaused} />
+                    <h2>ALERT: Low chances criminals will be arrested</h2>
+                  </div>
+              }
+            </div>
+        }
+      </Modal>
+      <Affix>
+        <Card style={{ backgroundColor: "#e6fffb" }}>
+          <PageHeader
+            // className="site-page-header"
+            // onBack={() => null}
+            title="Criminal Arrest Predictor"
+            subTitle="An assistant to help find out the strength of police force needed to be deployed on a particular Community Area on the basis of chances of arrest of the criminals"
+          />
+        </Card>
+      </Affix>
+      <div style={{
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+        marginTop: "10vh"
+      }}>
+        <Card style={{
+          backgroundColor: "#f0f0f0",
+          width: "80vh",
+        }}>
+          <Form
+            {...layout}
+            onFinish={async val => {
+              const modelInputData = {
+                'community_area': val['community_area'],
+                'day': dayReverseMap[val['day']],
+                'month': monthReverseMap[val['month']],
+                'domestic': val['domestic'],
+                'primary_type': val['primary_type'],
+              };
+              setModalLoading(true);
+              setIsModalVisible(true);
+              const res = await axios.post(apiUrl, modelInputData);
+              setArrested(res.data.arrested);
+              setModalLoading(false);
+            }}
+            onFinishFailed={err => console.log(err)}
           >
-            <Select showSearch>
-              {communityAreaField.map(val =>
-                <Option value={val}>{val}</Option>
-              )}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label="Day of the week"
-            name="day"
-            rules={[
-              {
-                required: true,
-                message: 'Please select the day'
-              },
-            ]}
-          >
-            <Select showSearch>
-              {dayField.map(val =>
-                <Option value={dayMap[val]}>{dayMap[val]}</Option>
-              )}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label="Month"
-            name="month"
-            rules={[
-              {
-                required: true,
-                message: 'Please select the month'
-              },
-            ]}
-          >
-            <Select showSearch>
-              {monthField.map(val =>
-                <Option value={monthMap[val]}>{monthMap[val]}</Option>
-              )}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label="Wether the crime will be domestic or not"
-            name="domestic"
-            rules={[
-              {
-                required: true,
-                message: 'Please select wether the crime is domestic or not'
-              },
-            ]}
-          >
-            <Select showSearch>
-              {domesticField.map(val =>
-                <Option value={val}>{val}</Option>
-              )}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label="Type of the crime"
-            name="primary_type"
-            rules={[
-              {
-                required: true,
-                message: 'Please select the type of the crime'
-              },
-            ]}
-          >
-            <Select showSearch>
-              {primaryTypeField.map(val =>
-                <Option value={val}>{val}</Option>
-              )}
-            </Select>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
+            <Form.Item
+              name="community_area"
+              label="Community Area"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select the community area',
+                },
+              ]}
+            >
+              <Select showSearch>
+                {communityAreaField.map(val =>
+                  <Option value={val}>{val}</Option>
+                )}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="Day of the week"
+              name="day"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select the day'
+                },
+              ]}
+            >
+              <Select showSearch>
+                {dayField.map(val =>
+                  <Option value={dayMap[val]}>{dayMap[val]}</Option>
+                )}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="Month"
+              name="month"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select the month'
+                },
+              ]}
+            >
+              <Select showSearch>
+                {monthField.map(val =>
+                  <Option value={monthMap[val]}>{monthMap[val]}</Option>
+                )}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="Domestic Crime"
+              name="domestic"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select wether the crime is domestic or not'
+                },
+              ]}
+            >
+              <Select showSearch>
+                {domesticField.map(val =>
+                  <Option value={val}>{val}</Option>
+                )}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="Type of the crime"
+              name="primary_type"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select the type of the crime'
+                },
+              ]}
+            >
+              <Select showSearch>
+                {primaryTypeField.map(val =>
+                  <Option value={val}>{val}</Option>
+                )}
+              </Select>
+            </Form.Item>
+            <Form.Item>
+              <div style={{
+                display: "flex",
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center"
+              }}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </div>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 }
