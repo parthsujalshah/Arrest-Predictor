fd9b7ff0f987b12f9f63258dc4dca2baa66c6ef7
parthsujalshah <parthsujalshah@gmail.com>
2021-11-20 01:18:51 +0530

    api working

--- app.py
+++ app.py
@@ -4,32 +4,56 @@ import joblib
 import pandas as pd
 import json
 
+from pandas.core.algorithms import mode
+
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route('/', methods=['POST'])
 def home():
-    model = joblib.load('dt.joblib')
     build_dict = {}
     f = open('cols.json')
     data = json.load(f)
+    print(request.json)
 
     pt = request.json.get('primary_type')
-    ca = request.data.get('community_area')
+    ca = request.json.get('community_area')
+    day = request.json.get('day')
+    month = request.json.get('month')
+    domestic = request.json.get('domestic')
 
 
     for i in data['column_names']:
         if 'Primary Type' in i:
             if pt in i:
-                build_dict[f'Primary Type_{pt}'] = [1]
+                build_dict[i] = [1]
             else:
-                build_dict[f'Primary Type_{pt}'] = [0]
+                build_dict[i] = [0]
         elif 'Community Area' in i:
             if ca in i:
-                build_dict[f'Community Area_{ca}'] = [1]
+                build_dict[i] = [1]
             else:
-                build_dict[f'Community Area_{ca}'] = [0]
+                build_dict[i] = [0]
+        elif 'Day' in i:
+            if day in i:
+                build_dict[i] = [1]
+            else:
+                build_dict[i] = [0]
+        elif 'Month' in i:
+            if month in i:
+                build_dict[i] = [1]
+            else:
+                build_dict[i] = [0]
+        elif 'Domestic' in i:
+            if domestic in i:
+                build_dict[i] = [1]
+            else:
+                build_dict[i] = [0]
+    df = pd.DataFrame.from_dict(build_dict)
+    model = joblib.load('dt.joblib')
+    pred = model.predict(df)
+    return jsonify({'will_be_arrested': int(pred[0])})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
